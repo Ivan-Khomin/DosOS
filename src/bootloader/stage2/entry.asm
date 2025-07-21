1d9e0582cf2d8e5423dc1042cc5df88805f4ca7b
Ivan-Khomin <homin151210@gmail.com>
2025-07-21 06:57:32 +0000

    Updated

--- src/bootloader/stage2/entry.asm
+++ src/bootloader/stage2/entry.asm
@@ -2,6 +2,9 @@ bits 16
 
 section .entry
 
+extern __bss_section
+extern __end
+
 extern start
 global entry
 
@@ -9,14 +12,22 @@ entry:
     cli
     mov [bootDrive], dl
 
+    ; setup segment registers
     mov ax, ds
     mov ss, ax
+
+    ; setup stack
     mov sp, 0
     mov bp, sp
-    
-    call EnableA20
     sti
 
+    ; clear bss
+    mov ecx, __end
+    mov edi, __bss_section
+    sub ecx, edi
+    mov al, 0
+    cld
+    rep stosb
 
     ; put boot drive in dl, send it as argument to start function
     xor dx, dx
@@ -26,66 +37,5 @@ entry:
 
     cli
     hlt
-
-EnableA20:
-    [bits 16]
-    ; disable keyboard
-    call A20WaitInput
-    mov al, KbdControllerDisableKeyboard
-    out KbdControllerCommandPort, al
-
-    ; read control output port
-    call A20WaitInput
-    mov al, KbdControllerReadCtrlOutputPort
-    out KbdControllerCommandPort, al
-
-    call A20WaitInput
-    in al, KbdControllerDataPort
-    push eax
-
-    ; write control output port
-    call A20WaitInput
-    mov al, KbdControllerWriteCtrlOutputPort
-    out KbdControllerCommandPort, al
-
-    call A20WaitInput
-    pop eax
-    or al, 2                                    ; bit 2 = A20 bit
-    out KbdControllerDataPort, al
-
-    ; enable keyboard
-    call A20WaitInput
-    mov al, KbdControllerEnableKeyboard
-    out KbdControllerCommandPort, al
-
-    call A20WaitInput
-
-    ret
-
-A20WaitInput:
-    [bits 16]
-    ; wait until status bit 2 (input buffer) is 0
-    ; by reading from command port, we read status byte
-    in al, KbdControllerCommandPort
-    test al, 2
-    jnz A20WaitInput
-
-    ret
-
-A20WaitOutput:
-    [bits 16]
-    ; wait until status bit 1 (output buffer) is 1 so it can be read
-    in al, KbdControllerCommandPort
-    test al, 1
-    jz A20WaitOutput
-
-    ret
-
-KbdControllerDataPort               equ 0x60
-KbdControllerCommandPort            equ 0x64
-KbdControllerEnableKeyboard         equ 0xAE
-KbdControllerDisableKeyboard        equ 0xAD
-KbdControllerReadCtrlOutputPort     equ 0xD0
-KbdControllerWriteCtrlOutputPort    equ 0xD1
 
 bootDrive:  db 0
--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -3,8 +3,6 @@
 #include "stdio.h"
 #include "disk.h"
 
-uint8_t far* data = (uint8_t far*)0x50000000;
-
 void ASMCALL start(uint16_t bootDrive)
 {
     Disk disk;
@@ -12,19 +10,6 @@ void ASMCALL start(uint16_t bootDrive)
     {
         printf("Cannot initialize disk!\n");
         goto end;
-    }
-
-    if (!Disk_ReadSectors(&disk, 19, 1, data))
-    {
-        printf("Cannot read data!\n");
-        goto end;
-    }
-
-    for (int i = 0; i < 5 * 32; i += 32)
-    {
-        for (int j = 0; j < 11; j++)
-            putc(data[i + j]);
-        putc('\n');
     }
 
 end:
